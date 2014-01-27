2b4e6ebd0e4506e31e125679f3c11651e1a55275
Greg Fitzgerald <greg@gregf.org>
2014-01-27 10:02:09 -0500

    Add a more generic knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,18 +1,35 @@
 current_dir = File.dirname(__FILE__)
+user_email  = `git config --get user.email`
+home_dir    = ENV['HOME'] || ENV['HOMEDRIVE']
+org         = ENV['chef_org'] || 'wgig'
+
+knife_override = "#{home_dir}/.chef/knife_override.rb"
+
+chef_server_url          "https://api.opscode.com/organizations/#{org}"
 log_level                :info
 log_location             STDOUT
-node_name                "gregf"
-client_key               "#{current_dir}/gregf.pem"
-validation_client_name   "wgig-validator"
-validation_key           "#{current_dir}/wgig-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/wgig"
-cache_type               'BasicFile'
-cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-chef_repo_path "#{current_dir}/../"
-repo_mode "everything"
-cookbook_path [ 
-  "#{current_dir}/../cookbooks", 
-  "#{current_dir}/../site-cookbooks"
-]
 
+# USERNAME is UPPERCASE in Windows, but lowercase in the Chef server,
+# so `downcase` it.
+node_name                ( ENV['USER'] || ENV['USERNAME'] ).downcase
+client_key               "#{home_dir}/.chef/#{node_name}.pem"
+cache_type               'BasicFile'
+cache_options( :path => "#{home_dir}/.chef/checksums" )
+
+validation_client_name   "#{org}-validator"
+validation_key           "#{current_dir}/#{org}-validator.pem"
+
+# We keep our cookbooks in separate repos under a ~/chef/cookbooks dir
+cookbook_path            ["#{current_dir}/../../../cookbooks"]
+cookbook_copyright       "Wallgig.net"
+cookbook_license         "Apache 2"
+cookbook_email           "#{user_email}"
+
+# Allow overriding values in this knife.rb
+Chef::Config.from_file(knife_override) if File.exist?(knife_override)
+
+# Tell chef-vault we are using chef server.
 knife[:vault_mode] = 'client'
+
+# knife essentials settings
+repo_mode "everything"
