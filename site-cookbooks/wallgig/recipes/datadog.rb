780b11f0b5ab57c2d927e1d8f2f10236beb918bb
Greg Fitzgerald <greg@gregf.org>
2014-01-28 22:27:33 -0500

    Fix monitoring for nginx_status and elasticsearch

--- site-cookbooks/wallgig/recipes/datadog.rb
+++ site-cookbooks/wallgig/recipes/datadog.rb
@@ -5,20 +5,25 @@ datadog_vault = chef_vault_item('secrets', 'datadog')
 node.set['datadog']['api_key'] = datadog_vault['api_key']
 node.set['datadog']['application_key'] = datadog_vault['application_key']
 
-# Chef handler
-include_recipe "datadog::dd-handler"
-
-# Redis integration
-if node['redis']
-  package "python-redis"
-  node.set['datadog']['redis']['server']['addr'] = node['redis']['bind']
-  node.set['redis']['server']['port'] = node['redis']['port']
+# Elasticsearch integration
+if node['elasticsearch']
+  node.set['datadog']['elasticsearch']['instances'] = [
+    'url' => "http://localhost:9200"
+  ]
 end
 
 # Nginx integration
 if node['nginx']
-  node.set['datadog']['nginx']['status_url'] = "http://127.0.0.1:8090/nginx_status/"
+  node.set['datadog']['nginx']['instances'] = [
+    'nginx_status_url' => "http://127.0.0.1:8090/nginx_status/"
+  ]
 end
+
+# Chef handler
+include_recipe 'datadog'
+include_recipe 'datadog::dd-handler'
+include_recipe 'datadog::nginx'
+include_recipe 'datadog::elasticsearch'
 
 # Agent config
 include_recipe "datadog::dd-agent"
