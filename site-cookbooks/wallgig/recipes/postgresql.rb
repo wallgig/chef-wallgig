0e94b2d5eaa455135b936af39aea59a5d9b84521
Greg Fitzgerald <greg@gregf.org>
2014-01-28 23:37:07 -0500

    Add a datadog user for postgres metric

--- site-cookbooks/wallgig/recipes/postgresql.rb
+++ site-cookbooks/wallgig/recipes/postgresql.rb
@@ -3,13 +3,15 @@ include_recipe 'postgresql::libpq'
 include_recipe 'postgresql::server'
 
 include_recipe 'chef-vault'
-vault = chef_vault_item('databasesecrets', 'wallgig')
 
-pg_user 'wallgig' do
-  privileges superuser: false, createdb: false, login: true
-  password vault['password']
+%w{datadog wallgig}.each do |user|
+  vault = chef_vault_item('databasesecrets', user)
+
+  pg_user 'wallgig' do
+    privileges superuser: false, createdb: false, login: true
+    password vault['password']
+  end
 end
-
 
 pg_database 'wallgig_production' do
   owner 'wallgig'
