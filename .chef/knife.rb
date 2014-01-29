89fb9e019988d82270cd33b96a96d2d80721017b
Greg Fitzgerald <greg@gregf.org>
2014-01-28 19:22:31 -0500

    Fix a couple mistakes in .knife.rb that broke the client_key

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,6 +2,7 @@ current_dir = File.dirname(__FILE__)
 user_email  = `git config --get user.email`
 home_dir    = ENV['HOME'] || ENV['HOMEDRIVE']
 org         = ENV['chef_org'] || 'wgig'
+user        = ENV['USER'].downcase
 
 knife_override = "#{home_dir}/.chef/knife_override.rb"
 
@@ -9,10 +10,8 @@ chef_server_url          "https://api.opscode.com/organizations/#{org}"
 log_level                :info
 log_location             STDOUT
 
-# USERNAME is UPPERCASE in Windows, but lowercase in the Chef server,
-# so `downcase` it.
-node_name                ( ENV['USER'] || ENV['USERNAME'] ).downcase
-client_key               "#{home_dir}/.chef/#{node_name}.pem"
+node_name                user
+client_key               "#{home_dir}/.chef/#{user}.pem"
 cache_type               'BasicFile'
 cache_options( :path => "#{home_dir}/.chef/checksums" )
 
@@ -20,7 +19,7 @@ validation_client_name   "#{org}-validator"
 validation_key           "#{current_dir}/#{org}-validator.pem"
 
 # We keep our cookbooks in separate repos under a ~/chef/cookbooks dir
-cookbook_path            ["#{current_dir}/../../../cookbooks"]
+cookbook_path            ["#{current_dir}/../cookbooks", "#{current_dir}/../site-cookbooks"]
 cookbook_copyright       "Wallgig.net"
 cookbook_license         "Apache 2"
 cookbook_email           "#{user_email}"
