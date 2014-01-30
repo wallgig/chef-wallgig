3d46b8c7c1be25be8ebc24770d489d19e6d11c6e
Greg Fitzgerald <greg@gregf.org>
2014-01-30 15:53:55 -0500

    Make sure ruby 2.0.0 is set as the global version

--- site-cookbooks/wallgig/recipes/ruby.rb
+++ site-cookbooks/wallgig/recipes/ruby.rb
@@ -1,8 +1,11 @@
 node.set['rbenv']['group_users'] = %w{wallgig toor}
 
-include_recipe "rbenv"
-include_recipe "rbenv::ruby_build"
+include_recipe 'rbenv'
+include_recipe 'rbenv::ruby_build'
 
-rbenv_ruby '2.0.0-p353'
+rbenv_ruby 'Ruby 2.0' do
+  ruby_verson '2.0.0-p353'
+  global true
+end
 
 rbenv_gem 'bundler'
