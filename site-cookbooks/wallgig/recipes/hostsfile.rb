aaf3dd194f6f57e49f20c7ec3970c8c3d218987c
Greg Fitzgerald <greg@gregf.org>
2014-02-07 19:51:37 -0500

    Hostsfile was not idempotent

--- site-cookbooks/wallgig/recipes/hostsfile.rb
+++ site-cookbooks/wallgig/recipes/hostsfile.rb
@@ -1,26 +1,26 @@
 hostsfile_entry '127.0.0.1' do
   hostname 'localhost'
-  action :create
+  action :create_if_missing
 end
 
 hostsfile_entry '108.59.1.209' do
   hostname 'www.wallgig.net'
   aliases ['wallgig.net', 'cdn.wallgig.net', 'api.wallgig.net']
-  action :create
+  action :create_if_missing
 end
 
 hostsfile_entry 'ff02::1' do
   hostname 'ip6-allnodes'
-  action :create
+  action :create_if_missing
 end
 
 hostsfile_entry 'ff02::2' do
   hostname 'ip6-allrouters'
-  action :create
+  action :create_if_missing
 end
 
 hostsfile_entry '::1' do
   hostname 'localhost'
   aliases ['ip6-localhost', 'ip6-loopback']
-  action :create
+  action :create_if_missing
 end
