68337fa1da75b964891b7c9cf4f5cc476305e9e2
Greg Fitzgerald <greg@gregf.org>
2014-02-15 21:42:25 -0500

    Rate limit logdrops and ssh connections.

--- site-cookbooks/wallgig/recipes/firewall.rb
+++ site-cookbooks/wallgig/recipes/firewall.rb
@@ -11,21 +11,32 @@ simple_iptables_rule 'state' do
   rule '-m conntrack --ctstate ESTABLISHED,RELATED'
   jump 'ACCEPT'
 end
+simple_iptables_rule 'logdrop' do
+  rule '-m limit --limit 5/m --limit-burst 10 -j LOG'
+  jump false
+end
+simple_iptables_rule "bad-bot" do
+  rule '--source 129.21.129.165'
+  jump "DROP"
+end
 simple_iptables_rule 'icmp' do
-  rule '-p icmp'
+  rule '--proto icmp'
   jump 'ACCEPT'
 end
 simple_iptables_rule 'ssh' do
-  rule '-p tcp --dport 22'
+  rule '--proto tcp --dport 22'
   jump 'ACCEPT'
+end
+simple_iptables_rule 'ssh' do
+  rule '-p tcp --dport 22 -m state --state NEW -m recent --set'
+  jump false
+end
+simple_iptables_rule 'ssh' do
+  rule '-p tcp --dport 22 -m state --state NEW -m recent --update --seconds 60 --hitcount 4'
+  jump 'REJECT'
 end
 simple_iptables_rule 'http' do
   rule [ "--proto tcp --dport 80",
         "--proto tcp --dport 443" ]
   jump 'ACCEPT'
-end
-
-simple_iptables_rule "bad-bot" do
-  rule '-s 129.21.129.165/32'
-  jump "DROP"
 end
