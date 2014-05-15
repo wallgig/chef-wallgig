0fa40ccaccc9bdbfc4fde644b1edbc06e08cb410
Greg Fitzgerald <greg@gregf.org>
2014-05-14 20:07:56 -0400

    Upgrade to ruby 2.1.2
    This upgrade includes swapping rvm out for ruby_install.

--- site-cookbooks/wallgig/recipes/ruby.rb
+++ site-cookbooks/wallgig/recipes/ruby.rb
@@ -1,5 +1,21 @@
-include_recipe 'rvm::system_install'
+include_recipe 'ruby_install'
 
-rvm_ruby '2.1.0'
-rvm_default_ruby '2.1.0'
-rvm_gem 'bundler'
+ruby_install_ruby 'ruby 2.1.2'
+gem_package 'bundler' do
+  gem_binary '/opt/rubies/ruby-2.1.2/bin/gem'
+  options '--no-ri --no-rdoc'
+end
+
+%w(ruby gem ri rdoc irb erb testrb rake bundle).each do |prg|
+  link "/usr/bin/#{prg}" do
+    to "/opt/rubies/ruby-2.1.2/bin/#{prg}"
+  end
+end
+
+file '/etc/profile.d/ruby.sh' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  content 'export PATH=/opt/rubies/ruby-2.1.2/bin:$PATH'
+  action :create
+end
