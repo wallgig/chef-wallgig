cc00245a8c3db0025ce896cbb3e3796f114874a5
Greg Fitzgerald <greg@gregf.org>
2014-06-14 21:52:57 -0400

    Cleanup qutoes on emailonreboot

--- site-cookbooks/wallgig/recipes/emailonreboot.rb
+++ site-cookbooks/wallgig/recipes/emailonreboot.rb
@@ -1,12 +1,12 @@
 cookbook_file '/etc/init.d/email-on-reboot' do
-    owner 'root'
-    group 'root'
-    mode '0755'
-    source 'email-on-reboot'
-    action :create
+  owner 'root'
+  group 'root'
+  mode '0755'
+  source 'email-on-reboot'
+  action :create
 end
 
 service 'email-on-reboot' do
-    supports :start => true, :stop => false, :restart => false
-    action [ :enable ]
+  supports :start => true, :stop => false, :restart => false
+  action [ :enable ]
 end
